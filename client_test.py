32daaf501308b6a2fcdf6e55429769a4eae9fb01
roy.lieu <roy.lieu@gmail.com>
2015-07-27 15:08:52 +0800

    modify printf format

---
+++ client_test.py
@@ -0,0 +1,34 @@
+import gevent
+from gevent import monkey
+monkey.patch_all()
+import sys
+import requests
+import datetime
+
+SHOW_ID = "143796412990036"
+SERVER_URL = "http://stage.bolo.me:34567/api/live_show"
+
+def send_heartbeat(client_no, show_id, cookie_str, sleep_time):
+    while 1:
+        data = {"show_id": show_id}
+        headers = {
+            "Cookie": "h5_user=" + cookie_str,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        print "client %d start, with cookie: %s" % (client_no, cookie_str)
+        resp = requests.post(SERVER_URL, data, headers=headers)
+        print "got response:", resp.headers
+        gevent.sleep(sleep_time)
+
+
+def start(num_of_clients):
+    task_pool = []
+    for i in range(num_of_clients):
+        cookie_str = datetime.datetime.now().strftime("%H%M%S%f")
+        task = gevent.spawn(send_heartbeat, i, SHOW_ID, cookie_str, 3)
+        task_pool.append(task)
+    gevent.wait(task_pool)
+
+
+if __name__ == "__main__":
+    start(int(sys.argv[1]))
